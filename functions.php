d6ffad376bfd2680ecdb128079741d299b1a425d
Thadius Hukari <thadiushukari@yahoo.com>
2017-04-27 17:49:00 -0700

    moved folder

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Theme Name: Web170 Sping 2017
+Theme URI: https://github.com/ThadiusHukari/WEB170-CMS-WordPress
+Author: Thadius Hukari
+Author URI: http://hukares.com/web170/wordpress/
+Description: Web170 Class class in Spring 2017 Quarter for Content Management Systems.
+Version: 42.0
+*/
+
+?>
